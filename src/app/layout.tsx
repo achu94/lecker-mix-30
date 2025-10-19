b4e232ba4643feb29cb63e60616e95abd7efd287
Achu Yakub <aygyun_94@outlook.de>
2025-10-19 12:12:13 +0200

    chore
    daisyui and awesomeFonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,7 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="cupcake">
+      <meta name="viewport" content="viewport-fit=cover" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
