4b98f3e5b423c183b57f8d3c38f9200fbca6f396
Achu Yakub <aygyun_94@outlook.de>
2025-10-27 00:43:39 +0100

    chore: thema changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="cupcake">
+    <html lang="en" data-theme="sunset">
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
